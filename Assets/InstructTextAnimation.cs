5619c2093b55d1baf7804c1ade43a2c97b8ba52d
Greenfield, Paige Alyssa <paigegreenfield6@gmail.com>
2019-04-09 17:23:14 -0400

    addsc

---
+++ Assets/InstructTextAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructTextAnimation : MonoBehaviour
+{
+    Animator anim;
+    public GameObject player;
+    PlayerMovement pMove;
+    Image image;
+    bool hasMoved;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.enabled = false;
+        anim = GetComponent<Animator>();
+        pMove = player.GetComponent<PlayerMovement>();
+        hasMoved = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pMove.controlsEnabled && !hasMoved)
+        {
+            image.enabled = true;
+            anim.enabled = true;
+            hasMoved = true;
+            anim.SetTrigger("FadeIn");
+        }
+        if(Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
+        {
+            anim.SetTrigger("FadeOut");
+        }
+    }
+    void GoAway()
+    {
+        image.enabled = false;
+        anim.enabled = false;
+    }
+}
---
+++ Assets/VideoScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoScript : MonoBehaviour
+{
+    public AudioSource clip1;
+    VideoPlayer bereave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bereave = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clip1.time >= 89.8f && clip1.time <= 90.1f && !bereave.isPlaying)
+        {
+            bereave.Play();
+        }
+    }
+}
