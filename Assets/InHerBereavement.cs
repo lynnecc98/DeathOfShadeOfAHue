cf30ad8e3397587bb34a33af07b340bbade2eb58
Greenfield, Paige Alyssa <paigegreenfield6@gmail.com>
2019-04-09 20:55:21 -0400

    HECK YEAH

---
+++ Assets/InHerBereavement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InHerBereavement : MonoBehaviour
+{
+    public AudioSource clip1;
+    Animator anim;
+    Image sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<Image>();
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+        sprite.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clip1.time >= 89.8f && clip1.time <= 90.1f)
+        //if (clip1.time >= 10.8f && clip1.time <= 11.1f)
+            {
+            anim.enabled = true;
+            sprite.enabled = true;
+        }
+    }
+
+    void EndAnim()
+    {
+        anim.enabled = false;
+        sprite.enabled = false;
+    }
+}
