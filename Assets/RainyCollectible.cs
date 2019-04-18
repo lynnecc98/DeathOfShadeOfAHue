8d0511dfbf77eac4710b6d768afc8268c7b1956d
Greenfield, Paige Alyssa <paigegreenfield6@gmail.com>
2019-04-18 15:38:00 -0400

    cOOL CHANGES

---
+++ Assets/RainyCollectible.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainyCollectible : MonoBehaviour
+{
+    public GameObject player;
+    bool fallen;
+    float point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fallen = false;
+        point = Random.Range(-2f, 7.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!fallen)
+        {
+            transform.Translate(Vector3.down * 0.05f, Space.World);
+            if (transform.position.y <= point)
+            {
+                Debug.Log("it happened!");
+                fallen = true;
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Debug.Log("snatched");
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Assets/Spawning.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject collectible;
+    public float spawnTime;
+    public Transform[] spawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(collectible, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+
+}
