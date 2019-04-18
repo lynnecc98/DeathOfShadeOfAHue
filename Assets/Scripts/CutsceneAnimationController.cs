0cdab1bf8809d31d8a8e53097a95d7ca9e8f1da9
Greenfield, Paige Alyssa <paigegreenfield6@gmail.com>
2019-04-17 20:10:44 -0400

    rainy stuf

--- Assets/Scripts/CutsceneAnimationController.cs
+++ Assets/Scripts/CutsceneAnimationController.cs
@@ -7,20 +7,15 @@ public class CutsceneAnimationController : MonoBehaviour
     Animator anim;
     public GameObject player;
     public PlayerMovement pMove;
-    public GameObject timer;
-    public GameObject spawner;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         pMove = player.GetComponent<PlayerMovement>();
-        
     }
-  
+
     void EnablePlayerControls()
     {
-        timer.SetActive(true);
-        spawner.SetActive(true);
         pMove.controlsEnabled = true;
         anim.SetTrigger("StayOnPlayer");
     }
